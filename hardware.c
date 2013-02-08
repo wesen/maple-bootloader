f124a55d1605e43c979c4cfa758ae079786000ee
Manuel Odendahl <wesen@ruinwesen.com>
2013-02-07 19:27:06 -0500

    only set the PER bit on flash CR

--- hardware.c
+++ hardware.c
@@ -234,7 +234,7 @@ void systemHardReset(void) {
 
 bool flashErasePage(u32 pageAddr) {
     u32 rwmVal = GET_REG(FLASH_CR);
-    rwmVal = FLASH_CR_PER;
+    rwmVal |= FLASH_CR_PER;
     SET_REG(FLASH_CR, rwmVal);
 
     while (GET_REG(FLASH_SR) & FLASH_SR_BSY) {}
@@ -298,7 +298,3 @@ void flashUnlock() {
     SET_REG(FLASH_KEYR, FLASH_KEY1);
     SET_REG(FLASH_KEYR, FLASH_KEY2);
 }
-
-
-
-
